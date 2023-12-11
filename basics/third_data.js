7a3b1c84d801ab9266045f21a1bf700c486ebd6f
kbg2206 <73578362+kbg2206@users.noreply.github.com>
2023-12-11 17:48:02 +0000

    data types

---
+++ basics/third_data.js
@@ -0,0 +1,21 @@
+// variable declare same as python
+
+"use strict" // treat all js code with new version or new standard
+
+
+// alert("hello") // we are using node js not browser this is done only in browser we can use this in different way in node js
+let name = "kushagra" //string
+let age =22 //integer (range 2^53)
+// We can use bigint data type for larger range mostly used in stock market website
+
+let isLoggedIn=true //boolean
+let temp=null // 0 is a temperature but when website is not able to fetch any temp then how can we find that 
+//null =>stand alone value 
+//undefined =>when value is not assigned to variable 
+
+//symbol (mainly used when we learn about react to indetify component is unique ,figma tools  )=?unique
+
+//typeof to find the data type
+console.log(typeof(null)) // null is object type
+console.log(typeof(undefined)) // undefined is already a type
+console.log(typeof(age))
