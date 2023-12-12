048227438e0eebf4040c2deebd36854cc7fa6948
kbg2206 <73578362+kbg2206@users.noreply.github.com>
2023-12-12 07:24:35 +0000

    operator

---
+++ basics/fifth_operation.js
@@ -0,0 +1,20 @@
+
+
+let value=3
+let negvalue = - value
+console.log(negvalue) // -3
+
+let s="hello"
+let t="world"
+console.log(s+t)//helloworld
+// +,*,-,/,,**,%,++,-- prefix and postfix opeartor
+
+
+console.log(1+"2",typeof(1+"2")) //12
+console.log("1"+2,typeof("1"+2)) //12
+console.log("1"+2+2,typeof("1"+2+2)) // 122
+console.log(1+2+"2",typeof(1+2+"2")) // 32 left to right 
+
+console.log(+true)//1
+// console.log(true+)// error
+console.log(+"") // 0
---
+++ basics/sixth_comparision.js
@@ -0,0 +1,20 @@
+// >,<,>=,<=,== ,!=
+
+
+// try to compare same data type 
+console.log("2">1) //"2" convert into number 
+console.log("02">1)
+
+
+console.log(null>0) // f
+console.log(null==0) // f here null is convert to NaN
+console.log(null>=0) // t  predictable result will not come // >= convert null to 0
+
+//with undefined also we can check but it will give always false 
+
+
+// === check value and data type is same or not
+
+console.log("2"==2) //t
+console.log("2"===2)//f
+console.log(2===2)//t
