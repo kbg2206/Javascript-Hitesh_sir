448ce7f502245464c27d455deece8515cf0cd757
kbg2206 <73578362+kbg2206@users.noreply.github.com>
2023-12-12 17:50:03 +0000

    number methods

---
+++ basics/lec12_numberMethods.js
@@ -0,0 +1,28 @@
+// const age=22
+
+// console.log(age.toString())
+// console.log(age.toString().length)
+// console.log(age.toFixed(5))
+
+// const num=123.7586218
+// console.log( num.toPrecision(4))
+
+
+// const hundred=100000000
+// console.log(hundred.toLocaleString('en-IN')) // according to indian standard bydefault us standard
+
+// console.log(Number.MIN_VALUE)
+
+
+//****************************maths****** inbuilt library
+
+const pi=Math.PI
+console.log(pi)
+
+console.log(Math.abs(-6-9))
+console.log(Math.round(5.6989))
+// ceil floor also their  
+//pow sqrt random
+//min  max from array
+
+console.log(Math.random()) // provide value betwwen 0 and 1
